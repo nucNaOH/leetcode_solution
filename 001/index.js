f2cc30d12b4063d7d0b1b0eb064b9feef2afbb00
王楠 <773013686@qq.com>
2017-10-04 11:06:07 +0800

    change tab to 4*space

--- 001/index.js
+++ 001/index.js
@@ -6,12 +6,12 @@
  */
 var twoSum = function(nums, target) {
     for(let i=0; i < nums.length; i++){
-		for(let j = i+1; j < nums.length;j++){
-			if(nums[i] + nums[j] === target) {
-				return [i, j];
-			}
-		} 
-	}
+        for(let j = i+1; j < nums.length;j++){
+            if(nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        } 
+    }
     return false;
 };
 
@@ -25,10 +25,10 @@ var twoSum = function(nums, target) {
     let m = new Map();
     for(let i=0; i < nums.length; i++){
         let tmp = target - nums[i];
-		if (m.has(tmp)) {
+        if (m.has(tmp)) {
             return [m.get(tmp), i];
         }
         m.set(nums[i], i);
-	}
+    }
     return false;
 };
